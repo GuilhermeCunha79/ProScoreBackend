29b23cf9f06454e113bd5db6746d7ff88099af17
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2023-04-28 19:15:48 +0100

    ç

---
+++ TestProject1/Tests/Controller/JogadorControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Controller;
+using ConsoleApp1.Domain.Forms;
+using Moq;
+using Xunit;
+
+namespace TestProject1.Tests.Controller;
+
+public class JogadorControllerTest
+{
+    [Fact]
+    public async void Create()
+    {
+        var deliveryServiceMock = new Mock<IJogadorService>();
+        string licenca = "2";
+        string estatuto = "Portugal";
+        string idPessoa = "132";
+        string idEquipa = "2342";
+        string status = "Active";
+        var delivery = new Jogador(licenca,estatuto,idPessoa);
+        var deliveryDto = new JogadorDTO(delivery.Id.AsGuid(), estatuto, licenca, idPessoa, idEquipa, status);
+
+        deliveryServiceMock.Setup (_ => _.GetByIdAsync (delivery.Id)).ReturnsAsync (deliveryDto);
+        deliveryServiceMock.Setup (_ => _.AddAsync (deliveryDto)).ReturnsAsync (deliveryDto);
+
+           
+        var controller = new JogadorController(deliveryServiceMock.Object);
+
+        var actual = await controller.Create(deliveryDto);
+
+        Assert.NotNull(actual);
+        Assert.NotNull(actual.Result);
+        
+    }
+}
